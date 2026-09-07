230ab1f2c30f4419b12313b697f980def929fc73
Mher DeLight <mahirzamil2012@gmail.com>
2026-09-07 22:05:54 +0300

    removed unnecessary comment

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,6 @@ int main() {
     auto draw = dez::DrawObject(mesh, dez::Transform(), GREEN);
     auto player = dez::PhysicsObject(std::move(draw), 0.5f);
     player.core.transform().position = Vector3{0.0f, 5.0f, 0.0f};
-    // DimEngineZ::physics::registerObject(&player);
 
     mesh = GenMeshCube(20.0f, 1.0f, 20.0f);
     draw = dez::DrawObject(mesh, dez::Transform({0.0f, -0.5f, 0.0f}), BROWN);
