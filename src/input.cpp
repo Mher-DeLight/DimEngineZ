c0e4a38237c972ecf184c6d6f3dd664705d7177b
Mher DeLight <mahirzamil2012@gmail.com>
2026-09-09 13:58:33 +0300

    implement input::getVector3

--- src/input.cpp
+++ src/input.cpp
@@ -9,5 +9,10 @@ Vec2 getVector(int keyNegX, int keyPosX, int keyNegY, int keyPosY) {
     return Vec2{static_cast<float>(getAxis(keyNegX, keyPosX)),
                 static_cast<float>(getAxis(keyPosY, keyNegY))};
 }
+Vec3 getVector3(int keyNegX, int keyPosX, int keyNegY, int keyPosY, int keyNegZ, int keyPosZ) {
+    return Vec3{static_cast<float>(getAxis(keyNegX, keyPosX)),
+                static_cast<float>(getAxis(keyPosY, keyNegY)),
+                static_cast<float>(getAxis(keyPosZ, keyNegZ))};
+}
 
 } // namespace DimEngineZ::input
