a6c9b31d1d386e735e8a5954a544bb5e0c10d7cf
Mher DeLight <mahirzamil2012@gmail.com>
2026-09-09 18:17:51 +0300

    changed space key press check to dez::input

--- test/main.cpp
+++ test/main.cpp
@@ -29,7 +29,7 @@ int main() {
         constexpr float SPEED = 10.0f;
         dez::Vec2 input = dez::input::getVector2() * SPEED;
 
-        if (IsKeyPressed(KEY_SPACE) && player.isOnGround) {
+        if (dez::input::isKeyPressed(KEY_SPACE) && player.isOnGround) {
             player.core.applyImpulse(Vector3{0.0f, JUMP_FORCE, 0.0f});
         }
         player.core.setHorizontalVelocity(input);
